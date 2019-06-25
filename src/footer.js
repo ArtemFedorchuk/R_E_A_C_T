998afc50c40d1d12997dd79b86ce2ea665ba3d4b
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-06-25 23:31:55 +0300

    react

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div class='footer'>
+            <pre>Fedorchuk Artem</pre>
+        </div>
+    );
+}
+
+export default Footer;
