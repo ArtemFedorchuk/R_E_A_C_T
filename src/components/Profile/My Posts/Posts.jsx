c3e6a63f26a0fb84dbfdd5d7e4532a30160c61bd
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-09-21 17:03:01 +0300

    load

--- src/components/Profile/My Posts/Posts.jsx
+++ src/components/Profile/My Posts/Posts.jsx
@@ -4,35 +4,37 @@ import Post from './Post/Post';
 
 
 
+
 const Posts = (props) => {
     // debugger
     // Функция которая мапит массив постов в новый массив
     let posts = props.state.profilePage.posts.map((p) => <Post text={p.message} author={p.auhor} />)
 
     let newPostElement = React.createRef();
+    let Dispatcher = require('react-dispatcher');
 
     let addPost = () => {
         
         let text = newPostElement.current.value
         // console.log(text)
         props.addPost(text)
-         console.log(text)
+        newPostElement.current.value = ''
     }
+
+    // store.dispach(addPost)
+
  
     return (
         <div className={p.post}>
             <div className="post__send">
                 {/* <hr/> */}
                 {/* <h4>My post</h4> */}
-                <input type="textarea" ref={newPostElement} placeholder="Напишите пост" /><br />
+                <input type="textarea" ref={newPostElement} placeholder="Напишите пост" value={props.newPostText} /><br />
                 <button type="submit" onClick={addPost}>Send</button>
             </div>
             <hr />
 
             {posts}
-            <Post title='First Post' state={props.state} />
-            {/* <Post title='Two Post' state={props.state} /> */}
-            {/* <Post title='Free Post' state={props.state} /> */}
         </div>
     );
 }
