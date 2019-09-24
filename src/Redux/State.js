80641b9872801fe34cdc32b680cd08fcc9e9f5d5
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-09-24 18:24:27 +0300

    refactor state.js

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -65,12 +65,13 @@ export let store = {
 
     },
     renderEntireTree () {
-        console.log('hello')
+        // console.log('hello')
     },
     addPost(postMessage) {
+        // debugger
         let newPost = {
             id: 5,
-            message: this.postMessage,
+            message: postMessage,
             likeCount: 45
         }
         this._state.profilePage.posts.push(newPost)
@@ -85,94 +86,5 @@ export let store = {
     }
 }
 
-// let renderEntireTree = () => console.log('hello')
-// let state = {
-
-//     profilePage: {
-//         // Информация профиля
-//         profileInfo: [
-//             { name: 'Vasso Kolbasso' },
-//             { special: 'Doctor-reanimate' },
-//             { date: '27.08.1990' },
-//             { town: 'California' },
-//             { schoоl: 'Harvard' },
-//             { website: 'stroyukrinvest.com' }
-//         ],
-//         // Массив постов
-//         posts: [
-//             // { id: 1, message: 'sdfsdgsfdgsg', likeCount: 23, author: 'Artem Fedorchuk' },
-//             // { id: 1, message: postMessage, likeCount: 121, author: 'Igor Shilov' },
-//             // { id: 1, message: postMessage, likeCount: 16, author: 'Alexandr Zelensky' },
-//             // { id: 1, message: postMessage, likeCount: 133, author: 'Vitalino Tarantino' }
-//         ],
-//         newPostText: ''
-
-//     },
-
-//     messagesPage: {
-//         // Массив диалогов
-//         dialogs: [
-//             { id: 1, name: 'Vasso' },
-//             { id: 2, name: 'Murces' },
-//             { id: 3, name: 'Adriano' },
-//             { id: 4, name: 'Victor' }
-//         ],
-//         // Массив сообщений.
-//         messages: [
-//             { text: 'Hi my brother' },
-//             { text: 'Hello my friend Artem.' },
-//             { text: 'Lomnis suscipit, fugit et earum quae neque necessitatibus velit magni?.' }
-//             // { text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.' }
-//         ]
-//     },
-
-//     newsPage: {
-//         // news content
-//     },
-
-//     musicPage: {
-//         // content music
-//     },
-
-//     settingPage: {
-//         // setting
-//     },
-
-//     navbar: {
-//         friends: [
-//             { name: 'Artem' },
-//             { name: 'Victor' },
-//             { name: 'Lucas' },
-//             { name: 'Valera' },
-//             { name: 'Nicolas' },
-//             // {name : 'Duglas'},
-//             { name: 'Djo' }
-//         ]
-//     }
-
-// }
-
-// export let addPost = (postMessage) => {
-//     // debugger
-//     let newPost = {
-//         id: 5,
-//         message: postMessage,
-//         likeCount: 45
-//     }
-//     state.profilePage.posts.push(newPost)
-//     renderEntireTree(state)
-// }
-
-// export let onChangeText = (newText) => {
-//     // debugger
-//     state.profilePage.newPostText = newText
-//     renderEntireTree(state)
-// }
-
-// export let subscribe = (subscriber) => {
-//     renderEntireTree = subscriber
-// }
-
-// export default state;
 export default store
 window.store = store
