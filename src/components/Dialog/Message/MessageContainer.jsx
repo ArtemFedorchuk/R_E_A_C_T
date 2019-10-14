ac4d0d2bd24d2844c35d4ffb26f51dd34f6ef4d6
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-10-14 12:31:32 +0300

    refactor MessageContainer component, and create connect

--- src/components/Dialog/Message/MessageContainer.jsx
+++ src/components/Dialog/Message/MessageContainer.jsx
@@ -1,24 +1,47 @@
-import React from 'react';
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../../Redux/State';
 import Message from './Message';
+import { connect } from 'tls';
 
 // sendMessageCreator
 // updateNewMessageBodyCreator
 
 
 
-const MessageContainer = (props) => {
-    // debugger
+// const MessageContainer = (props) => {
+//     // debugger
 
-    let newMessageBody = props.state.messagesPage.newMessageBody;
-    let onSendMessageClick = () => {
-        props.dispatch(sendMessageCreator())
+//     let newMessageBody = props.state.messagesPage.newMessageBody;
+//     let onSendMessageClick = () => {
+//         props.dispatch(sendMessageCreator())
+//     }
+//     // Func input text
+//     let onNewMessageChange = (e) => {
+//         let body = e.target.value
+//         props.dispatch(updateNewMessageBodyCreator(body))
+//     }
+//     return (<Message onSendMessageClick={onSendMessageClick} onNewMessageChange={onNewMessageChange} newMessageBody={newMessageBody} />)
+// }
+// export default MessageContainer;
+
+
+let mapStateToProps = (state) => {
+    return {
+        newMessageBody: state.messagesPage.newMessageBody
     }
-    // Func input text
-    let onNewMessageChange = (e) => {
-        let body = e.target.value
-        props.dispatch(updateNewMessageBodyCreator(body))
-    }
-    return (<Message onSendMessageClick={onSendMessageClick} onNewMessageChange={onNewMessageChange} newMessageBody={newMessageBody} />)
 }
-export default MessageContainer;
+
+let mapDispatchToProps = (dispatch) => {
+    return {
+        onSendMessageClick: () => {
+            dispatch(sendMessageCreator())
+        },
+        onNewMessageChange: (e) => {
+            let body = e.target.value
+            dispatch(updateNewMessageBodyCreator(body))
+        }
+    }
+}
+
+const MessageContainer = connect(mapStateToProps, mapDispatchToProps)(Message)
+
+export default MessageContainer
