2cb444c4e025b3145c37ca6ca6cc5f4897dc184d
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-10-14 12:49:12 +0300

    refactor

--- src/components/Dialog/Message/MessageContainer.jsx
+++ src/components/Dialog/Message/MessageContainer.jsx
@@ -2,28 +2,6 @@ import { sendMessageCreator, updateNewMessageBodyCreator } from '../../../Redux/
 import Message from './Message';
 import { connect } from 'tls';
 
-// sendMessageCreator
-// updateNewMessageBodyCreator
-
-
-
-// const MessageContainer = (props) => {
-//     // debugger
-
-//     let newMessageBody = props.state.messagesPage.newMessageBody;
-//     let onSendMessageClick = () => {
-//         props.dispatch(sendMessageCreator())
-//     }
-//     // Func input text
-//     let onNewMessageChange = (e) => {
-//         let body = e.target.value
-//         props.dispatch(updateNewMessageBodyCreator(body))
-//     }
-//     return (<Message onSendMessageClick={onSendMessageClick} onNewMessageChange={onNewMessageChange} newMessageBody={newMessageBody} />)
-// }
-// export default MessageContainer;
-
-
 let mapStateToProps = (state) => {
     return {
         newMessageBody: state.messagesPage.newMessageBody
