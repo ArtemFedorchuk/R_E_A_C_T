910628372aa9cebf627647da168d720f031e1f62
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-10-22 19:18:44 +0300

    ref

--- src/Redux/user-Reducer.js
+++ src/Redux/user-Reducer.js
@@ -2,13 +2,13 @@ const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 
 let initialState = {
-        // Массив диалогов
-        users: [
-            { id: 1, fullname: 'Vasso', located: {country: 'Ukraine', town: 'Kharkov'}, followStatus: true, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss' },
-            { id: 2, fullname: 'Borya', located: {country: 'Ukraine', town: 'Kiev'}, followStatus: false, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss two' },
-            { id: 3, fullname: 'Tioman', located: {country: 'Ukraine', town: 'Dnepr'}, followStatus: true, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss three' },
-            { id: 4, fullname: 'Murces', located: {country: 'Ukraine', town: 'Odessa'}, followStatus: false, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss four' }
-        ]
+    // Массив диалогов
+    users: [
+        { id: 1, fullname: 'Vasso', located: { country: 'Ukraine', town: 'Kharkov' }, followStatus: true, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss' },
+        { id: 2, fullname: 'Borya', located: { country: 'Ukraine', town: 'Kiev' }, followStatus: false, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss two' },
+        { id: 3, fullname: 'Tioman', located: { country: 'Ukraine', town: 'Dnepr' }, followStatus: true, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss three' },
+        { id: 4, fullname: 'Murces', located: { country: 'Ukraine', town: 'Odessa' }, followStatus: false, faceId: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss four' }
+    ]
 }
 
 const userReducer = (state = initialState, action) => {
@@ -17,14 +17,14 @@ const userReducer = (state = initialState, action) => {
         case FOLLOW:
             return {
                 ...state,
-                newMessageBody:  action.body
+                newMessageBody: action.body
                 // newMessageBody: [...state.newMessageBody, action.body],
                 // newMessageBody: action.body
             }
-            // stateCopy.newMessageBody = {...state.newMessageBody}
-            // stateCopy.newMessageBody = action.body
-            // return stateCopy
-        
+        // stateCopy.newMessageBody = {...state.newMessageBody}
+        // stateCopy.newMessageBody = action.body
+        // return stateCopy
+
         case UNFOLLOW:
             return {
                 ...state,
@@ -33,11 +33,11 @@ const userReducer = (state = initialState, action) => {
                 messages: [...state.messages, { text: action.body }]
 
             }
-            // stateCopy.newMessageBody = {...state.newMessageBody}
-            // stateCopy.newMessageBody = ''
-            // state.messages.push({ text: stateCopy.newMessageBody })
-            // return stateCopy
-        
+        // stateCopy.newMessageBody = {...state.newMessageBody}
+        // stateCopy.newMessageBody = ''
+        // state.messages.push({ text: stateCopy.newMessageBody })
+        // return stateCopy
+
         default:
             return state
     }
