2a65cd7f0d9a022458c3831096fdecb2b81abacf
ArtemFedorchuk <43110065+ArtemFedorchuk@users.noreply.github.com>
2019-10-24 01:41:23 +0300

    refactor

--- src/Redux/user-Reducer.js
+++ src/Redux/user-Reducer.js
@@ -4,7 +4,7 @@ const UNFOLLOW = 'UNFOLLOW'
 let initialState = {
     // Массив диалогов
     users: [
-        { id: 1, fullname: 'Vasso', located: { country: 'Ukraine', town: 'Kharkov' }, followStatus: true, src: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss' },
+        { id: 1, fullname: 'Vasso Teroristo', located: { country: 'Ukraine', town: 'Kharkov' }, followStatus: true, src: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss' },
         { id: 2, fullname: 'Borya', located: { country: 'Ukraine', town: 'Kiev' }, followStatus: false, src: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss two' },
         { id: 3, fullname: 'Tioman', located: { country: 'Ukraine', town: 'Dnepr' }, followStatus: true, src: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss three' },
         { id: 4, fullname: 'Murces', located: { country: 'Ukraine', town: 'Odessa' }, followStatus: false, src: 'https://proimg.ru/wp-content/uploads/2017/10/8cb.jpg', aboutMe: 'I am a Boss four' }
@@ -35,7 +35,7 @@ const userReducer = (state = initialState, action) => {
                     return u
                 })
             }
-            
+
             default: 
             return state
     }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,13 +4,38 @@ import u from './Users.module.css'
 const Users = (props) => {
     debugger
 
+
     return (
         <div>
             <div><h3 className={u.h3}>Users</h3></div>
-            <div>
-                <image src="props.src" alt="user-photo"/>
+            <div className = { u.wrap_cont }>
+                <div className={u.user_wrap}>
+                    <img src="https://www.ptichka.ru/data/cache/2018mar/18/35/64148_60448.jpg" className={u.img} alt="user" />
+                    <h2 className={u.h2}>{props.users[0].fullname}</h2>
+                    <p className={u.located}>{props.users[0].located.country}, {props.users[0].located.town}</p>
+                    <p className={u.status}>{props.users[0].aboutMe}</p>
+                </div>
+                <div className={u.user_wrap}>
+                    <img src="https://www.ptichka.ru/data/cache/2018mar/18/35/64148_60448.jpg" className={u.img} alt="user" />
+                    <h2 className={u.h2}>{props.users[0].fullname}</h2>
+                    <p className={u.located}>{props.users[0].located.country}, {props.users[0].located.town}</p>
+                    <p className={u.status}>{props.users[0].aboutMe}</p>
+                </div>
+                <div className={u.user_wrap}>
+                    <img src="https://www.ptichka.ru/data/cache/2018mar/18/35/64148_60448.jpg" className={u.img} alt="user" />
+                    <h2 className={u.h2}>{props.users[0].fullname}</h2>
+                    <p className={u.located}>{props.users[0].located.country}, {props.users[0].located.town}</p>
+                    <p className={u.status}>{props.users[0].aboutMe}</p>
+                </div>
+                <div className={u.user_wrap}>
+                    <img src="https://www.ptichka.ru/data/cache/2018mar/18/35/64148_60448.jpg" className={u.img} alt="user" />
+                    <h2 className={u.h2}>{props.users[0].fullname}</h2>
+                    <p className={u.located}>{props.users[0].located.country}, {props.users[0].located.town}</p>
+                    <p className={u.status}>{props.users[0].aboutMe}</p>
+                </div>{/*  */}
             </div>
         </div>
     )
 }
+
 export default Users
