8b2cce49d52b38c8eb5d425c466b8d2eb91e06f3
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2019-11-20 11:32:28 +0200

    create NavLink in FriendNav navbar menu

--- src/components/Navbar/FriendsNav/FriendNav/FriendNav.jsx
+++ src/components/Navbar/FriendsNav/FriendNav/FriendNav.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import f from './../FriendNav/FriendNav.module.css'
+import { NavLink } from "react-router-dom";
+
 
 const FriendNav = (props) => {
     // debugger
     return (
-        <div>
-            <div className={f.user}>
-                <img src="http://klakson18.ru/wp-content/uploads/2016/12/06.png" alt="user"/>
-                <p>{props.name}</p>
+        <NavLink to='user/2 '>
+            <div>
+                <div className={f.user}>
+                    <img src="http://klakson18.ru/wp-content/uploads/2016/12/06.png" alt="user"/>
+                    <p>{props.name}</p>
+                </div>
             </div>
-        </div>
+        </NavLink>
     )
 }
 
